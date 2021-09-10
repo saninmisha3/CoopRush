cb9368c7daef83e37bab038dc5fe8e35f1b6ce4f
misahh <saninmisha3@gmail.com>
2021-09-10 11:44:55 +0200

    Weapon Launcher

---
+++ Source/CoopGame/Public/Weapons/SLauncherWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/SBaseWeapon.h"
+#include "SLauncherWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPGAME_API ASLauncherWeapon : public ASBaseWeapon
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CoopGame/Public/Weapons/SRifleWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/SBaseWeapon.h"
+#include "SRifleWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COOPGAME_API ASRifleWeapon : public ASBaseWeapon
+{
+	GENERATED_BODY()
+	
+};
