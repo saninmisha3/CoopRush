452c162d216aea79498c18a75954f37fee9d4f37
misahh <saninmisha3@gmail.com>
2021-09-25 14:21:05 +0200

    Spawner GameMode

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -2,3 +2,42 @@
 
 #include "SGameMode.h"
 
+ASGameMode::ASGameMode()
+{
+    NrOfBotsToSpawn = 4;
+    WaveCount = 2;
+    TimerBetweenWaves = 5.f;
+    NrOfSpawnedBots = 0;
+}
+
+void ASGameMode::StartPlay()
+{
+    Super::StartPlay();
+    PrepareForNextWave();
+}
+
+void ASGameMode::PrepareForNextWave()
+{
+    if(--WaveCount < 0) return;
+    FTimerHandle BetweenWavesTimer;
+    GetWorldTimerManager().SetTimer(BetweenWavesTimer, this, &ASGameMode::StartWave, TimerBetweenWaves);
+}
+
+void ASGameMode::StartWave()
+{
+    GetWorldTimerManager().SetTimer(SpawnBotsTimer, this, &ASGameMode::OnSpawnBotsTimer,1.f, true, 0.f);
+}
+
+void ASGameMode::OnSpawnBotsTimer()
+{
+    if(++NrOfSpawnedBots >= NrOfBotsToSpawn) EndWave();
+    SpawnBot();
+}
+
+
+void ASGameMode::EndWave()
+{
+    GetWorldTimerManager().ClearTimer(SpawnBotsTimer);
+    NrOfSpawnedBots = 0;
+    PrepareForNextWave();
+}
--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -10,5 +10,31 @@ UCLASS()
 class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+public:
+    ASGameMode();
+
+    virtual void StartPlay() override;
 	
+protected:
+    FTimerHandle SpawnBotsTimer;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Waves")
+    int32 NrOfBotsToSpawn;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Waves")
+    int32 WaveCount;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Waves")
+    float TimerBetweenWaves;
+
+    int32 NrOfSpawnedBots;
+    
+    UFUNCTION(BlueprintImplementableEvent, Category="Waves")
+    void SpawnBot();
+    void StartWave();
+    void EndWave();
+    void PrepareForNextWave();
+    void OnSpawnBotsTimer();
+    
 };
